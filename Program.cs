72109cfef976956d46f47bdf6389f8ed0503c847
Ole Martinsen <okmartinsen@gmail.com>
2018-08-28 13:46:42 +0200

    Second Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Testproject
+{
+    class MainClass {
+        static void Main(string[] args) {
+            System.Console.WriteLine("Testline");
+            System.Console.WriteLine("Finally, it works!");
+            System.Console.WriteLine("(I Hope it does)");
+        }
+    }
+}
